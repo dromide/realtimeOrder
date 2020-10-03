9e1c7cdc1c31eb9789e50c2941e06f11e7aec090
dromide <mz.hisia@gmail.com>
2020-10-03 10:43:12 +0900

    initial

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const ejs = require('ejs');
+
+const path = require('path');
+
+const expressLayout = require('express-ejs-layouts');
+
+const PORT = process.env.PORT || 3000;
+// if (process.env.PORT) { PORT = process.env.PORT; } else { PORT = 300; }
+
+app.get('/', (req, res) => {
+    //res.send('Hello from server');
+    res.render('home');
+});
+
+// set Template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+// app.listen(3000, () => {
+app.listen(PORT, () => {
+    console.log(`Listening on port xyz ${PORT}`);
+});
